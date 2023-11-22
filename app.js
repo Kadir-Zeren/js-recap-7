001e935097afd884d2c183dd7fcb5bac0bbcb077
kadir-zeren <k4dzeren@gmail.com>
2023-11-22 08:24:35 +0300

    v2

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-let str1 = "this is a string";
-let str2 = `this is a string`;
-let str3 = "this is a string";
-let str4 = new String("This is a string");
+// let str1 = "this is a string";
+// let str2 = `this is a string`;
+// let str3 = "this is a string";
+// let str4 = new String("This is a string");
 
-console.log(typeof str1);
-console.log(typeof str2);
-console.log(str3);
-console.log(str4);
+// console.log(typeof str1);
+// console.log(typeof str2);
+// console.log(str3);
+// console.log(str4);
 
-console.log(str3[0]);
+// console.log(str3[0]);
+
+const str = "Hello World";
+console.log(str.length);
+console.log(str[6]);
+
+for (let i = 0; i < str.length; i++) {
+  console.log(str[i]);
+}
+
+for (let i = str.length; i > 0; i--) {
+  console.log(str[i - 1]);
+}

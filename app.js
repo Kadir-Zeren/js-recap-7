4e135173d71b068d7e3f9a40d66f4f8f8c4a2a34
kadir-zeren <k4dzeren@gmail.com>
2023-11-22 08:20:10 +0300

    v1

---
+++ app.js
@@ -0,0 +1,11 @@
+let str1 = "this is a string";
+let str2 = `this is a string`;
+let str3 = "this is a string";
+let str4 = new String("This is a string");
+
+console.log(typeof str1);
+console.log(typeof str2);
+console.log(str3);
+console.log(str4);
+
+console.log(str3[0]);
